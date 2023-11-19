f8dbe676b193dcf97c3e54eea5042ac0b6a80263
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-19 16:09:25 +0000

    output image has transparent background & is png

--- image_handler.py
+++ image_handler.py
@@ -26,14 +26,14 @@ def find_unique_colors(image_path):
     return unique_colors
 
 # Crops an image by colour
-
-def crop_image_by_color(original_img_path, target_color, output_path,tolerance=10):
+# TODO: Create new tolerance for each of R G B
+def crop_image_by_color(original_img_path, target_color, output_path,tolerance=5):
 
     original_img = Image.open(original_img_path)
 
     width, height = original_img.size
 
-    new_img = Image.new("RGB",(width, height), "white")
+    new_img = Image.new("RGBA", (width, height), (0, 0, 0, 0))
 
     for x in range(width):
         for y in range(height):
@@ -43,4 +43,4 @@ def crop_image_by_color(original_img_path, target_color, output_path,tolerance=1
             if all(abs(p - t) <= tolerance for p, t in zip(pixel, target_color)):
                 new_img.putpixel((x, y), pixel)
 
-    new_img.save(output_path)
+    new_img.save(output_path, format="PNG")
