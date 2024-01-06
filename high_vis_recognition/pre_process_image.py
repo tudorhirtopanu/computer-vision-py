18b94942be212f32f5f3ba3da1204611bfc78de6
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-06 00:38:56 +0000

    apply gaussian blur to image

--- high_vis_recognition/pre_process_image.py
+++ high_vis_recognition/pre_process_image.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter
+import cv2
 import numpy as np
 
 # Resize the image
@@ -10,12 +11,32 @@ def resize_image(input_path, target_width, target_height):
 
     return resized_image
 
-image_input_path = './test_images/parking_warden_3.jpg'
-image_output_path = './high_vis_recognition/pre_processed_images/pre_processed_image.jpg'
+def apply_blur(input_image, kernel):
+    
+    image = np.array(input_image)
+
+    blurred_image_array = cv2.filter2D(image, -1, kernel)
+
+    blurred_image = Image.fromarray(blurred_image_array)
+
+    return blurred_image
+
+
+image_input_path = './test_images/high_vis_test.jpeg'
 image_output_path_base = './high_vis_recognition/pre_processed_images/'
 
 target_width = 512
 target_height = 512
+
+gaussian_blur_kernel_3x3 = np.array(([1,2,1],
+                                 [2,4,2],
+                                 [1,2,1])) / 16
+
+gaussian_blur_kernel_5x5 =  np.array([[1, 4, 7, 4, 1],
+                                     [4, 16, 26, 16, 4],
+                                     [7, 26, 41, 26, 7],
+                                     [4, 16, 26, 16, 4],
+                                     [1, 4, 7, 4, 1]]) / 273.0
 
 def preprocess_image(image_input_path, target_width, target_height):
 
@@ -23,9 +44,9 @@ def preprocess_image(image_input_path, target_width, target_height):
 
     resized_image.save(image_output_path_base+'resized_image.jpg')
 
-    noise_reduced_image = resized_image.filter(ImageFilter.GaussianBlur(1))
+    blurred_image = apply_blur(resized_image, gaussian_blur_kernel_3x3)
 
-    noise_reduced_image.save(image_output_path_base+'noise_reduced_image.jpg')
+    blurred_image.save(image_output_path_base+'gaussian_blur.jpg')
 
     return resized_image
 
