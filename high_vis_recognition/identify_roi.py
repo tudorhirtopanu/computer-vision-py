6d7a1e1861e5da5452c878505a47aa00db836c3e
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-13 16:40:50 +0000

    filter by contour size

--- high_vis_recognition/identify_roi.py
+++ high_vis_recognition/identify_roi.py
@@ -6,40 +6,65 @@ import matplotlib.pyplot as plt
 image_input_path = './high_vis_recognition/pre_processed_images/gaussian_blur.jpg'
 image_output_path_base = './high_vis_recognition/roi_images/'
 
+lower_color_green = np.array([30, 100, 100])
+upper_color_green = np.array([40, 255, 255])
+
 # image to be a numpy array
 def return_edges(image):
 
-    edges = cv2.Canny(image, 50, 150)
-    edges_image = Image.fromarray(edges)
-    edges_image.save(image_output_path_base + 'edges.jpg')
+    edges = cv2.Canny(image, 90, 150)
+    #edges_image = Image.fromarray(edges)
+    #edges_image.save(image_output_path_base + 'edges.jpg')
     return edges
 
 def colour_based_segmentation(image_array):
 
     hsv_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2HSV)
 
-    lower_color_green = np.array([30, 100, 100])
-    upper_color_green = np.array([40, 255, 255])
+    #lower_color_orange = np.array([10, 100, 100])
+    #upper_color_orange = np.array([20, 255, 255])   
 
-    lower_color_orange = np.array([10, 100, 100])
-    upper_color_orange = np.array([20, 255, 255])   
+    #mask_green = cv2.inRange(hsv_image, lower_color_green, upper_color_green) 
+    #mask_orange = cv2.inRange(hsv_image, lower_color_green, upper_color_green) 
 
-    mask_green = cv2.inRange(hsv_image, lower_color_green, upper_color_green) 
-    mask_orange = cv2.inRange(hsv_image, lower_color_orange, upper_color_orange) 
+    #final_mask = cv2.bitwise_or(mask_green, mask_orange)
 
-    final_mask = cv2.bitwise_or(mask_green, mask_orange)
+    color_mask = cv2.inRange(hsv_image, lower_color_green, upper_color_green)
 
-    #color_mask = cv2.inRange(hsv_image, lower_color, upper_color)
-
-    segmented_image = cv2.bitwise_and(image_array, image_array, mask=final_mask)
+    segmented_image = cv2.bitwise_and(image_array, image_array, mask=color_mask)
 
     return segmented_image
 
+def get_contours(segmented_image, min_contour_area=150):
+    # Find contours in the binary image
+    contours, _ = cv2.findContours(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filter contours based on area
+    filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+
+    # Create an empty mask
+    mask = np.zeros_like(segmented_image)
+
+    # Draw contours on the mask
+    cv2.drawContours(mask, filtered_contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+
+    # Apply the mask to the original image
+    result_image = cv2.bitwise_and(segmented_image, mask)
+
+    return result_image
+
 def identify_roi(preprocessed_image_array):
+    edges = return_edges(preprocessed_image_array)
+    segmented_image = colour_based_segmentation(preprocessed_image_array)
 
-    image_array = colour_based_segmentation(preprocessed_image_array)
+    # Filter edges based on the presence of segmented color
+    edges_with_color = cv2.bitwise_and(edges, edges, mask=cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY))
 
-    return image_array
+    combined_image = cv2.addWeighted(segmented_image, 1, cv2.cvtColor(edges_with_color, cv2.COLOR_GRAY2BGR), 1, 0)
+
+    filer_by_contour = get_contours(combined_image)
+
+    return filer_by_contour, combined_image
 
     
 
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import cv2
 import numpy as np
 from high_vis_recognition.pre_process_image import preprocess_image
-from high_vis_recognition.identify_roi import identify_roi, colour_based_segmentation
+from high_vis_recognition.identify_roi import identify_roi, return_edges
 
-input_image_path = "test_images/construction_site_1.jpg"
-target_width = 512
-target_height = 512
+input_image_path = "test_images/parking_warden_1.jpg"
+target_width = 300
+target_height = 300
 
 original_image = cv2.imread(input_image_path)
 
 preprocessed_image = preprocess_image(input_image_path, target_width, target_height)
 
-segmented_image = identify_roi(preprocessed_image)
+edges_image = return_edges(preprocessed_image)
+
+filtered_image, segmented_image = identify_roi(preprocessed_image)
 
 cv2.imshow("original image", original_image)
+cv2.imshow("edges", edges_image)
 cv2.imshow("image", segmented_image)
+cv2.imshow("filtered image", filtered_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
